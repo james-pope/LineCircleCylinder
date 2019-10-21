56f5a944e297dc504c662e9ff66e77660808566c
james-pope <jamespope@billericak12.com>
2019-10-21 11:04:08 -0400

    updated

--- src/com/company/Cylinder.java
+++ src/com/company/Cylinder.java
@@ -4,19 +4,17 @@ public class Cylinder {
 
     private Circle base;
     private double height;
-    private double radius;
 
         public Cylinder(int r, int h){
             base = new Circle(r);
             height = h;
-            radius = r;
         }
         public double vol(){
-            double a = Math.PI * (radius * radius) * height;
+            double a = base.area() * height;
             return a;
         }
         public double sa(){
-            double a = (2 * Math.PI * radius * height) + (2 * Math.PI * (radius * radius));
+            double a = (2 *(base.area()) + (base.circumference()*height));
             return a;
         }
     }
